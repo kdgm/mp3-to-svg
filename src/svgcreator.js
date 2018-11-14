aee66ce12e5959fe1cc03890cf62213f4085dfb7
Wouter Bin <wouterbin@gmail.com>
2018-11-14 15:10:02 +0100

    use kdgm green as color

--- src/svgcreator.js
+++ src/svgcreator.js
@@ -9,7 +9,7 @@ class SvgCreator {
 
   buildSVG(path, numberOfSamples) {
     return new Promise((resolve, reject) => {
-      const tempsvg = `<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg preserveAspectRatio="none" viewBox="0 -1 ${numberOfSamples} 2" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;"><path stroke-width="0.7" stroke="#000" d="${path}" fill="none" height="100%" width="100%" x="0" y="0" /></svg>`;
+      const tempsvg = `<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg preserveAspectRatio="none" viewBox="0 -1 ${numberOfSamples} 2" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;"><path stroke-width="0.7" stroke="#39b54a" d="${path}" fill="none" height="100%" width="100%" x="0" y="0" /></svg>`;
       resolve(tempsvg);
     });
   }
