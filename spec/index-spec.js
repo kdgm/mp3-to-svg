77bfbfc3c8057f08862b89759c68957c5218da2d
Wouter Bin <wouterbin@gmail.com>
2018-11-22 14:58:19 +0100

    add spec with a fixture to test

---
+++ spec/index-spec.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const glob = require('glob');
+const chai = require('chai');
+const expect = chai.expect;
+const convertMP3toSVG = require('../src/index.js');
+
+describe('Main', function() {
+  context('mainly', function() {
+
+    afterEach(function(){
+      let files = glob.sync('spec/**/*.svg');
+      files.forEach( (file) => {
+        fs.unlink(file);
+      });
+    });
+
+    it('should create one SVG file from an audio file', async function() {
+      await convertMP3toSVG('spec/fixtures/test.mp3', 'spec/fixtures/test.svg', [400])
+      expect(fs.existsSync('spec/fixtures/test400.svg')).to.be.true;
+    });
+
+    it('should create two SVG files from an audio file', async function() {
+      await convertMP3toSVG('spec/fixtures/test.mp3', 'spec/fixtures/test.svg', [100, 400])
+      expect(fs.existsSync('spec/fixtures/test400.svg')).to.be.true;
+      expect(fs.existsSync('spec/fixtures/test100.svg')).to.be.true;
+    });
+  });
+});
