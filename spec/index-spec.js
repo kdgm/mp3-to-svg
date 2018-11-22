a786a5137372c52cdd3d7ad305806490dc18590f
Wouter Bin <wouterbin@gmail.com>
2018-11-22 15:35:45 +0100

    missing semicolons

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -15,12 +15,12 @@ describe('Main', function() {
     });
 
     it('should create one SVG file from an audio file', async function() {
-      await convertMP3toSVG('spec/fixtures/test.mp3', 'spec/fixtures/test.svg', [400])
+      await convertMP3toSVG('spec/fixtures/test.mp3', 'spec/fixtures/test.svg', [400]);
       expect(fs.existsSync('spec/fixtures/test400.svg')).to.be.true;
     });
 
     it('should create two SVG files from an audio file', async function() {
-      await convertMP3toSVG('spec/fixtures/test.mp3', 'spec/fixtures/test.svg', [100, 400])
+      await convertMP3toSVG('spec/fixtures/test.mp3', 'spec/fixtures/test.svg', [100, 400]);
       expect(fs.existsSync('spec/fixtures/test400.svg')).to.be.true;
       expect(fs.existsSync('spec/fixtures/test100.svg')).to.be.true;
     });
