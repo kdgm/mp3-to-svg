0067456e496a8c2a30cf2b4aeb38e64b2df750e1
Wouter Bin <wouterbin@gmail.com>
2018-11-29 12:47:19 +0100

    satisfy eslint

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -1,36 +1,36 @@
+/* eslint-env mocha */
+/* eslint-disable prefer-arrow-callback, func-names, no-unused-expressions */
 const fs = require('fs');
 const glob = require('glob');
-const chai = require('chai');
-const expect = chai.expect;
+const { expect } = require('chai');
 const convertMP3toSVG = require('../src/index.js');
 
-describe('Main', function() {
-  context('mainly', function() {
 
-    afterEach(function(){
-      let files = glob.sync('spec/**/*.svg');
-      files.forEach( (file) => {
+describe('Main', function () {
+  context('mainly', function () {
+    afterEach(function () {
+      const files = glob.sync('spec/**/*.svg');
+      files.forEach((file) => {
         fs.unlinkSync(file);
       });
     });
 
-    it('should create one SVG file from an audio file', async function() {
+    it('should create one SVG file from an audio file', async function () {
       await convertMP3toSVG('spec/fixtures/test.mp3', 'spec/fixtures/test.svg', [400]);
       expect(fs.existsSync('spec/fixtures/test400.svg')).to.be.true;
     });
 
-    it('should create two SVG files from an audio file', async function() {
+    it('should create two SVG files from an audio file', async function () {
       await convertMP3toSVG('spec/fixtures/test.mp3', 'spec/fixtures/test.svg', [100, 400]);
       expect(fs.existsSync('spec/fixtures/test400.svg')).to.be.true;
       expect(fs.existsSync('spec/fixtures/test100.svg')).to.be.true;
     });
 
-    it('should create two SVG files from an audio file', async function() {
+    it('should create two SVG files from an audio file', async function () {
       await convertMP3toSVG('spec/fixtures/test.mp3', 'spec/fixtures/test.svg', [400]);
       await fs.stat('spec/fixtures/test400.svg', (err, stats) => {
         expect(stats.size).to.equal(10046);
       });
     });
-
   });
 });
