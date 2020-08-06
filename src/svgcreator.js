6e438595d628ba6c369604bab7973b2070f18390
Davita <davita@kerkdienstgemist.nl>
2020-08-06 10:24:44 +0200

    Change stroke colour to pastel green

--- src/svgcreator.js
+++ src/svgcreator.js
@@ -13,7 +13,7 @@ class SvgCreator {
 
   buildSVG(path, numberOfSamples) {
     return new Promise((resolve) => {
-      const tempsvg = `<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg preserveAspectRatio="none" viewBox="0 -1 ${numberOfSamples} 2" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;"><path stroke-width="0.7" stroke="#39b54a" d="${path}" fill="none" height="100%" width="100%" x="0" y="0" /></svg>`;
+      const tempsvg = `<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg preserveAspectRatio="none" viewBox="0 -1 ${numberOfSamples} 2" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;"><path stroke-width="0.7" stroke="#7cb584" d="${path}" fill="none" height="100%" width="100%" x="0" y="0" /></svg>`;
       resolve(tempsvg);
     });
   }
