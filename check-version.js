1489a80adc1a1eb546f4bfd9636cec1c2f9f43b8
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2022-11-28 15:01:50 +0100

    add requirements-check to check for required node and npm version

---
+++ check-version.js
@@ -0,0 +1,24 @@
+import semver from 'semver';
+import { exec } from 'child_process';
+import { engines } from './package.json';
+
+// check engines.node.version
+const requiredNodeVersion = engines.node;
+const actualNodeVersion = process.version;
+if (!semver.satisfies(actualNodeVersion, requiredNodeVersion)) {
+  console.log(`Required node version ${requiredNodeVersion} not satisfied with current version ${actualNodeVersion}.`);
+  process.exit(1);
+}
+
+// check engines.npm.version
+const requiredNpmVersion = engines.npm;
+exec(
+  'npm -v',
+  (error, stdout) => {
+    const actualNpmVersion = stdout;
+    if (!semver.satisfies(actualNpmVersion, requiredNpmVersion)) {
+      console.log(`Required node version ${requiredNpmVersion} not satisfied with current version ${actualNpmVersion}.`);
+      process.exit(1);
+    }
+  },
+);
